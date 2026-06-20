90b9efc9078f9a2f63b056a83ba369518e89bce3
Chandra Gunawan <beyonddd.games@gmail.com>
2026-06-20 23:31:25 +0700

    Load dialogue data done

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,45 +1,56 @@
 using UnityEngine;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+
+using DialogueData = System.Collections.Generic.Dictionary<
+    Session,
+    System.Collections.Generic.List<
+        System.Collections.Generic.Dictionary<
+            Speaker,
+            System.Collections.Generic.List<string>
+        >
+    >
+>;
+
+public enum Session
+{
+    NPC_1,
+    NPC_2,
+}
+
+public enum Speaker
+{
+    PLAYER,
+    ENEMY
+
+}
 
 public class DialogueManager : MonoBehaviour
 {
-    public enum Session
-    {
-        NPC_1,
-        NPC_2,
-    }
 
-    public enum Speaker
-    {
-        PLAYER,
-        ENEMY
-    }
+    private DialogueData dialogueData;
 
-
-    [System.Serializable]
-    public class Data
-    {
-        public Dictionary<string, List<string>> dialogue;
-    }
-
-    [System.Serializable]
-    public class DataWrapper
-    {
-        public Dictionary<string, Data[]> allData;
-    }
     void Awake()
+    {
+        LoadDialogueData();
+    }
+
+    private void LoadDialogueData()
     {
         TextAsset jsonFile = Resources.Load<TextAsset>("Dialogue");
         if (jsonFile != null)
         {
-            DataWrapper dataWrapper = JsonUtility.FromJson<DataWrapper>(jsonFile.text);
-            Debug.Log("??? : " + dataWrapper);
+            dialogueData = JsonConvert.DeserializeObject<DialogueData>(jsonFile.text);
+            Debug.Log("??? : " + dialogueData[Session.NPC_1][0][Speaker.PLAYER][0]);
+
+            Debug.Log("[INFO] Dialgoue data succesfully loaded");
         }
         else
         {
             Debug.LogError("Failed to locate the JSON file in the Resources folder!");
         }
     }
+
 }
 
 
